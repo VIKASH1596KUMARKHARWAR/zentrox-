5e41e6174c979dee0b59a50f79cae233fe6caeaf
Vikash Kumar Kharwar <130039516+VIKASH1596KUMARKHARWAR@users.noreply.github.com>
2025-04-05 18:30:59 +0530

    Update page.tsx

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic"
+
+
 import ProfileForm from '@/components/forms/profile-form'
 import React from 'react'
 import ProfilePicture from './_components/profile-picture'
