09092bff10b080d31a902555c8599b299cb209e5
Vikash Kumar Kharwar <130039516+VIKASH1596KUMARKHARWAR@users.noreply.github.com>
2025-08-16 12:38:01 +0530

    Update page.tsx

--- src/app/(main)/(pages)/dashboard/page.tsx
+++ src/app/(main)/(pages)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const DashboardPage = (props) => {
+const DashboardPage = () => {
   // Dynamic Greeting based on time
   const currentHour = new Date().getHours();
   const greeting =
